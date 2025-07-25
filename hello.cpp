72458b5f51e5894fbd793fa5d85bf4cf8360e169
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-07-25 15:04:50 +0100

    feat: comments and multi-line commments

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,15 @@
 #include "iostream"
 #include <string>
 
+/* Returns a simple greeting when invoked */
 std::string greet() { return "Hello"; }
 
+// entry point of our program
 int main() {
+
+  // uses the cout object from the i/o stream library
+  // to dispay a message on the command line
   std::cout << "Hello, World!";
 
-  return 0;
+  return 0; // zero indicates a successful execution
 }
