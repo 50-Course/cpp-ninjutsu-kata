3d12fd27a344bf98b7a8f1e409a182ab90ecb8da
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-07-25 18:55:12 +0100

    feat: working with headers

---
+++ header_kata.cpp
@@ -0,0 +1,13 @@
+// working with headers
+//
+// Headers define informaiton on how to do a tasks, its usually the placec
+// to hold directives and directive-related inforamtion in larger projects
+
+#include "header_kata.h"
+
+int main() {
+  // do nothing - but return successfully
+
+  cout << "Hello, world!" << endl;
+  return 0;
+}
---
+++ header_kata.h
@@ -0,0 +1,7 @@
+// defines the directives that we would use in our 
+// header_kata file
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
