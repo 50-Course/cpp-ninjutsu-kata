5e20edb1a40b87bea0393dccb59fc9d2f1c122db
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-07-25 15:16:39 +0100

    updates

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 /* Returns a simple greeting when invoked */
-std::string greet() { return "Hello"; }
+std::string greet(std::string name) { return "Hello, world"; }
 
 // entry point of our program
 int main() {
@@ -12,11 +12,19 @@ int main() {
   std::cout << "Hello, World!";
 
   int age = 0;
+  std::string name;
 
   std::cout << "\nEnter your age: ";
   // reads the user input from the command line
   // and stores it in the variable 'age'
   std::cin >> age;
+
+  std::cout << "\nWhat\'s your name? ";
+  std::cin >> name;
+
+  // greets the user using the greet function to display
+  // their name and age
+  greet(name);
 
   return 0; // zero indicates a successful execution
 }
